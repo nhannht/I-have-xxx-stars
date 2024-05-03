32ba95e47db590cbedd5ad09141bd7d0dbaf5b54
nhannht <nhanclassroom@gmail.com>
2024-05-04 02:41:08 +0700

    feat: init

---
+++ src/main/java/dev/nhannht/Secret.java
@@ -0,0 +1,16 @@
+package dev.nhannht;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+class Secret {
+    String github_oauth_repo1;
+
+}
---
+++ src/main/java/dev/nhannht/SecretManager.java
@@ -0,0 +1,37 @@
+package dev.nhannht;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+
+public class SecretManager {
+
+    public String getSecret (){
+        String secretName = "secret_vault1";
+    Region region = Region.of("ap-southeast-1");
+
+    // Create a Secrets Manager client
+    SecretsManagerClient client = SecretsManagerClient.builder()
+            .region(region)
+            .build();
+
+    GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+            .secretId(secretName)
+            .build();
+
+    GetSecretValueResponse getSecretValueResponse;
+
+    try {
+        getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+    } catch (Exception e) {
+        // For a list of exceptions thrown, see
+        // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        throw e;
+    }
+
+    String secret = getSecretValueResponse.secretString();
+    return secret;
+
+    }
+}
